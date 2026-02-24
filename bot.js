32be194472fc5cd8fbc2534bd6fdb09fd5c3b790
augustopradocf <augustopradof@gmail.com>
2026-02-24 02:26:55 -0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,17 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = "COLE_SEU_TOKEN_AQUI";
+
+const bot = new TelegramBot(token, { polling: true });
+
+console.log("Bot iniciado...");
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  if (msg.text === '/start') {
+    bot.sendMessage(chatId, '🔥 Bot ADS 2026.1 ativo!\n\nDigite o nome da música 🎵');
+  } else {
+    bot.sendMessage(chatId, `Você digitou: ${msg.text}`);
+  }
+});
