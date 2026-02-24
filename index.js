6f2d52938c87635336f1fc2181000f034438b523
augustopradocf <augustopradof@gmail.com>
2026-02-24 02:42:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 const TelegramBot = require('node-telegram-bot-api');
+const yts = require('yt-search');
+const ytdl = require('ytdl-core');
+const fs = require('fs');
 
 const token = "8612845950:AAGb8-sgRn0nd5gXLxlGreVxTU7P5Kk6bu8";
 
 const bot = new TelegramBot(token, { polling: true });
 
-console.log("Bot iniciado...");
-
-bot.on('message', (msg) => {
+bot.onText(/\/play (.+)/, async (msg, match) => {
   const chatId = msg.chat.id;
+  const query = match[1];
 
-  if (msg.text === '/start') {
-    bot.sendMessage(chatId, '🔥 Bot ADS 2026.1 ativo!\n\nDigite o nome da música 🎵');
-  } else {
-    bot.sendMessage(chatId, `Você digitou: ${msg.text}`);
+  bot.sendMessage(chatId, "🔎 Buscando música...");
+
+  try {
+    const search = await yts(query);
+    const video = search.videos[0];
+
+    if (!video) {
+      return bot.sendMessage(chatId, "❌ Música não encontrada.");
+    }
+
+    const stream = ytdl(video.url, { filter: 'audioonly' });
+
+    bot.sendAudio(chatId, stream, {
+      title: video.title
+    });
+
+  } catch (err) {
+    console.log(err);
+    bot.sendMessage(chatId, "❌ Erro ao buscar música.");
   }
+});
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "🔥 Bot DJ ADS ativo!\nUse /play nome da música 🎵");
 });
